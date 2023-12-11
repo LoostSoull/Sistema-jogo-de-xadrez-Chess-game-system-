b6ea9bea716e457e17dbe00e7a41a01bf138c0e1
gajeel redfox <lucas.silva.cardoso1996@gmail.com>
2023-12-11 11:10:22 -0300

    implementado tabuleiro e peças

---
+++ src/boardGame/Piece.java
@@ -0,0 +1,21 @@
+package boardGame;
+
+public class Piece {
+
+	protected Position position;
+	private Board board;
+	
+	
+	public Piece(Board board) {
+	
+		this.board = board;
+	}
+
+
+	public Board getBoard() {
+		return board;
+	}
+
+
+	
+}

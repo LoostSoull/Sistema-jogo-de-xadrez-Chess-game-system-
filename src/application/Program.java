8e45ec4a3cd26c38d9732988369e6583e6ba0e0a
gajeel redfox <lucas.silva.cardoso1996@gmail.com>
2023-12-16 09:11:37 -0300

    adicionado logica de checkmate

--- src/application/Program.java
+++ src/application/Program.java
@@ -17,7 +17,9 @@ public class Program {
 		Scanner sc = new Scanner(System.in);
 		ChessMatch chessMatch = new ChessMatch();
 		List<ChessPiece> captured = new ArrayList<>();
-		while (true) {
+		
+		
+		while (!chessMatch.getCheckMate()) {
 
 			try {
 				UI.clearScreen();
@@ -47,5 +49,8 @@ public class Program {
 				sc.nextLine();
 			}
 		}
+		
+		UI.clearScreen();
+		UI.printMatch(chessMatch, captured);
 	}
 }
